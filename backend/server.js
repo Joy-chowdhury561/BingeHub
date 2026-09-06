4c0c49fedf01618f9090d382bc824c275fc32cbc
joy chowdhury <mjoy24236@gmail.com>
2026-09-06 10:44:43 +0600

    added backend origin

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,41 @@
-import express from "express"
-import { connectDB } from "./lib/connectDB.js"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import authRoutes from "./routes/authRoutes.js"
-import movieRoutes from "./routes/movieRoutes.js"
-import TVroutes from "./routes/TVRoutes.js"
-import searchRoutes from "./routes/searchRoutes.js"
-import job from "./utils/cron.js"
-import cors from "cors"
-dotenv.config()
-const port=process.env.PORT || 5000
-const app=express()
-app.use(cors({
-    origin:"https://binge-hub-ten.vercel.app",
-    credentials:true,
-}))
+import express from "express";
+import { connectDB } from "./lib/connectDB.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
+import movieRoutes from "./routes/movieRoutes.js";
+import TVroutes from "./routes/TVRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import job from "./utils/cron.js";
+import cors from "cors";
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+const allowedOrigins = [
+  "https://binge-hub-ten.vercel.app",
+  "https://bingehub-backend-api-h.onrender.com",
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 app.set("trust proxy", 1);
-app.use(express.json())
-app.use(cookieParser()) 
-app.use(express.urlencoded({extended:true}))
-app.use("/api/auth",authRoutes)
-app.use("/api/movie",movieRoutes)
-app.use("/api/tv",TVroutes)
-app.use("/api/search",searchRoutes)
-app.get("/health",(req,res)=>{
-    res.status(200).json("ok")
-})
-app.listen(port,()=>{
-    console.log("the server started at port:",port);
-    connectDB()
-    if(process.env.NODE_ENV==="production"){
-        job.start()
-    }
-})
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/movie", movieRoutes);
+app.use("/api/tv", TVroutes);
+app.use("/api/search", searchRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).json("ok");
+});
+app.listen(port, () => {
+  console.log("the server started at port:", port);
+  connectDB();
+  if (process.env.NODE_ENV === "production") {
+    job.start();
+  }
+});
