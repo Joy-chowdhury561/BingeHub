592ae2f2ecb969e4cf2961ddbfc9675f0158f6e4
joy chowdhury <mjoy24236@gmail.com>
2026-09-07 20:32:39 +0600

    changed the mobile navbar button color

--- frontendnextjs/components/mobileNav.jsx
+++ frontendnextjs/components/mobileNav.jsx
@@ -28,17 +28,17 @@ const MobileNav = () => {
     <>
       <div className="w-full p-2.5 flex items-center justify-evenly bg-gray-950 rounded-t-2xl  sticky sm:hidden bottom-0 right-0 left-0">
         <Link href="/">
-          <div className="text-gray-700 text-[clamp(0.8rem,1vw,2rem)] flex flex-col items-center">
+          <div className="text-gray-500 text-[clamp(0.8rem,1vw,2rem)] flex flex-col items-center">
             <IoMdHome
-              className={`${page === "home" ? "text-white" : "text-gray-700"} text-3xl`}
+              className={`${page === "home" ? "text-green-500 shadow-[0_0_30px_rgba(34,197,94,0.7)]" : "text-gray-500"} text-3xl`}
             />
             Home
           </div>
         </Link>
         <Link href="/movies">
-          <div className="text-gray-700 text-[clamp(0.8rem,1vw,2rem)] flex flex-col items-center">
+          <div className="text-gray-500 text-[clamp(0.8rem,1vw,2rem)] flex flex-col items-center">
             <BiSolidMoviePlay
-              className={`${page === "movies" ? "text-white" : "text-gray-700"} text-3xl`}
+              className={`${page === "movies" ? "text-green-500 shadow-[0_0_30px_rgba(34,197,94,0.7)]" : "text-gray-500"} text-3xl`}
             />
             Movies
           </div>
@@ -46,7 +46,7 @@ const MobileNav = () => {
         <Link href="/tvs">
           <div className="text-gray-700 text-[clamp(0.8rem,1vw,2rem)] flex flex-col items-center">
             <PiFilmReelFill
-              className={`${page === "tv shows" ? "text-white" : "text-gray-700"} text-3xl`}
+              className={`${page === "tv shows" ? "text-green-500 shadow-[0_0_30px_rgba(34,197,94,0.7)]" : "text-gray-500"} text-3xl`}
             />
             Tv shows
           </div>
