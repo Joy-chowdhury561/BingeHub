93ea98194a745291108ae8028469f0ae0f3f9976
joy chowdhury <mjoy24236@gmail.com>
2026-09-15 20:19:21 +0600

    FUCKKKK

--- frontendnextjs/components/adBanner1.jsx
+++ frontendnextjs/components/adBanner1.jsx
@@ -1,7 +1,23 @@
-import AdsterraBanner from "./adsterraBanner";
+import Script from "next/script";
 
 const AdBanner = () => {
-  return <AdsterraBanner adKey="771fa7ec71dfcac85fc54951d92b999f" width={468} height={60} />;
+  return (
+    <div className="flex min-h-[60px] w-full justify-center overflow-hidden">
+      <Script id="adsterra-banner-options" strategy="afterInteractive">
+        {`var atOptions = {
+  key: "771fa7ec71dfcac85fc54951d92b999f",
+  format: "iframe",
+  height: 60,
+  width: 468,
+  params: {}
+};`}
+      </Script>
+      <Script
+        src="https://heavinessslight.com/771fa7ec71dfcac85fc54951d92b999f/invoke.js"
+        strategy="afterInteractive"
+      />
+    </div>
+  );
 };
 
 export default AdBanner;
