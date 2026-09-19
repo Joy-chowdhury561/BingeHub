32eafaa7e2533b3c71f851b67620718f13f47a1d
joy chowdhury <mjoy24236@gmail.com>
2026-09-19 16:24:13 +0600

    back to it

--- frontendnextjs/app/watch/tv/[tvId]/watchTv.jsx
+++ frontendnextjs/app/watch/tv/[tvId]/watchTv.jsx
@@ -45,7 +45,7 @@ const WatchTv = () => {
       </div>
 
       {!fetchingSimilars && (
-        <Category className="mt-3" category={similarTvs} categoryName={"You may also like"} />
+        <Category  category={similarTvs} categoryName={"You may also like"} />
       )}
       <Footer isPending={isPending}/>
     </>
